608f952bdf95a66a42bfab71b29b726fe497ae6e
jack ogina <jackogina60@gmail.com>
2018-11-11 19:36:26 +0300

    branch init

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/jakhax/grpc-golang-example/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func main() {
+
+	var conn *grpc.ClientConn
+	creds, err := credentials.NewClientTLSFromFile("/home/n1ght0wl/go/src/github.com/jakhax/grpc-golang-example/cert/server.crt", "")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	conn, err = grpc.Dial("localhost:7777", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal("Unable to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	c := api.NewPingClient(conn)
+
+	res, err := c.SayHello(context.Background(), &api.PingMessage{Greeting: "Hello world Server"})
+	if err != nil {
+		log.Fatal("Error occurred when sending message: %v", err)
+	}
+	log.Printf("Response: %s", res.Greeting)
+}
